23efeda7a6c612db317463aca22068d2159ca99b
kakapo <936555313@qq.com>
2020-08-24 13:39:40 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <tab-bar>
+        <Tab-Bar-Item>
+          <div></div>
+        <template v-slot:item-icon>
+               <img  src="./assets/img/TabBar/Homeblack.svg" alt="">
+        </template>
+        <template v-slot:item-text>
+             <div>首页</div>
+        </template>
+      </Tab-Bar-Item>
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+import TabBar from './components/tabbar/TabBar'
+import TabBarItem from './components/tabbar/TabBarItem' 
+
+export default {
+  name: 'App',
+  components: {
+     TabBar,
+     TabBarItem
+  }
+}
+
+</script>
+ 
+<style>
+ @import "./assets/css/base.css";
+</style>
